5ae1aca096960ae15e8c749b16531b0dc6bc3a10
yash16jr <yash16jr@gmail.com>
2023-05-25 01:07:17 +0530

    templates

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -15,7 +15,7 @@ from utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
+    preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
 
 class DataTransformation:
     def __init__(self):
